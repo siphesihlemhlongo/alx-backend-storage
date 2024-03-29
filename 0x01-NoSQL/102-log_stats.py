8172bc53008207f41165662d9a083894c8fa6959
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2024-03-29 12:49:58 -0700

    NoSQL

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,27 +1,40 @@
 #!/usr/bin/env python3
 """
-Top Students
+Log stats
 """
 from pymongo import MongoClient
 
 
-def top_students(mongo_collection):
-    pipeline = [
-        {"$unwind": "$scores"},
-        {"$group": {
-            "_id": "$_id",
-            "averageScore": {"$avg": "$scores.score"}
-        }},
-        {"$sort": {"averageScore": -1}}
-    ]
-    result = mongo_collection.aggregate(pipeline)
-    return list(result)
+def log_stats():
+    """ log_stats.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{total} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
+    print("IPs:")
+    sorted_ips = logs_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}  # Limit to the top 10 IPs
+    ])
+    for i, s in enumerate(sorted_ips, start=1):
+        print(f"\t{i}. {s.get('_id')}: {s.get('count')}")
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    students_collection = client.db.students
-
-    top_students_list = top_students(students_collection)
-    for student in top_students_list:
-        print(student)
+    log_stats()
